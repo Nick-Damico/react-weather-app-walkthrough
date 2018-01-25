f2246c7fb1e4dcd7ad59da37fe7dca00559c9c13
Nicholas D'Amico <nickalan82@icloud.com>
2018-01-25 08:22:18 -0500

    added CurrentForecast component imported 'react' and 'moment'

---
+++ src/components/CurrentForecast.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import moment from 'moment';
+
+function tempRound(temp) {
+  return Math.round(temp);
+}
+
+const CurrentForecast = ({forecast: { apparentTemperature, humidity, precipProbability, summary, temperature, time }}) =>
+  <div className="currentforecast__div">
+    <h2>Current Forecast</h2>
+    <div className="currentforecast__div--inner" style={{border: 'solid 1px black', padding: 16, margin: 16}}>
+      <h3>{summary}</h3>
+      <p>Time of Forecast: { time }</p>
+      <p>Temperature: { tempRound(temperature) }</p>
+      <p>Feels Like: { tempRound(apparentTemperature) }</p>
+      <p>Humidity: {humidity}</p>
+      <p>Chance of Percip: {precipProbability}%</p>
+    </div>
+  </div>
+
+export default CurrentForecast;
