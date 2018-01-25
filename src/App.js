28dab0cd139db307c2e92349531237d76daccc73
Nicholas D'Amico <nickalan82@icloud.com>
2018-01-25 08:23:47 -0500

    importing CurrentForecast component, added 'fetchJsonp' making request to darksky API, updated .env file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import CurrentForecast from './components/CurrentForecast';
+
+import fetchJsonp from 'fetch-jsonp';
 import './App.css';
 
-const API_URL = `https://api.darksky.net/forecast/${process.env.DARK_SKY_KEY}`;
+const API_URL = `https://api.darksky.net/forecast/${process.env.REACT_APP_DARK_SKY_KEY}/`;
 
 class App extends Component {
   constructor() {
@@ -17,30 +20,33 @@ class App extends Component {
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(pos => {
       const { latitude, longitude} = pos.coords;
-      console.log(latitude);
-      console.log(longitude);
 
-      fetch(`${API_URL}${latitude},${longitude}`)
+      fetchJsonp(`${API_URL}${latitude},${longitude}`)
         .then(resp => resp.json())
-        .then(forecast => console.log(forecast))
+        .then(weatherData => {
+          this.setState({
+            weatherData: weatherData,
+            fetchingData: false
+          })
+        })
     });
   }
 
 // How do I get my lat and long
   render() {
-    const { fetchingData } = this.state;
+    const { fetchingData, weatherData } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <h2>React Weather</h2>
         </header>
-        <p className="App-intro">
+        <div className="App-intro">
           {
             fetchingData
             ? <img src={logo} className="App-logo" alt="React Logo" />
-            : <h3>Data fetch complete</h3>
+            : <CurrentForecast forecast={weatherData.currently} />
           }
-        </p>
+        </div>
       </div>
     );
   }
